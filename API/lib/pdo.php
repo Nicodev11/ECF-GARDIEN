aa480cbe14e001d1357ba19db92b2c495b03dab1
Nicolas Gardien <Nicolasgardien.developpeur@gmail.com>
2023-02-23 16:54:05 +0100

    Add Symfony

--- API/lib/pdo.php
+++ API/lib/pdo.php
@@ -2,9 +2,11 @@
 $date = date('j F Y : G:i:s');
 
 try {
-  $pdo = new PDO('mysql:host=localhost;dbname=restaurant', 'root', 'NicoJuju69');
-} catch(Exception $e) {
-  file_put_contents('../logger/errorsdb.log', $date.' : '.$e->getMessage().PHP_EOL, FILE_APPEND); ?>
+  $pdo = new PDO('mysql:host=localhost', 'root', ''); ?>
+  <h1>Félicitations</h1>
+  <p>Vous êtes connecté à la base de données</p> <?php
+} catch (Exception $e) {
+  file_put_contents('../logger/errorsdb.log', $date . ' : ' . $e->getMessage() . PHP_EOL, FILE_APPEND); ?>
   <h1>Impossible de se connecter à la base de données</h1>
   <p>Merci de contacter l'administrateur du site</p> <?php
 }
